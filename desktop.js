a173f81eea91b333e4db1d0c29bcd742ea27d675
Parthiv <pparthivprakash1@gmail.com>
2025-06-09 23:26:37 +0530

    feat: before update

---
+++ desktop.js
@@ -0,0 +1,95 @@
+const products= [
+    {
+        id:1,
+        name:"MOTOROLA Razr 60 (PANTONE Lightest Sky, 256 GB)",
+        rating:4.6,
+        ratingCount:20358,
+        reviewsCount:1191,
+        specs:[
+            "6 GB RAM | 256 GB ROM",
+            "17.53 cm (6.9 inch) Full HD+ Display",
+            "50MP + 13MP | 32MP Front Camera",
+            "4500 mAh Battery",
+            "Dimensity 7400X Processor",
+            "1 Year Warranty on Handset and 6 Months Warranty on Accessories"
+        ],
+        originalPrice: 54999,
+        discountedPrice: 49999,
+        discount: "9% off",
+        freeDelivery: true,
+        exchangeOffer: "Upto ₹41,000 Off on Exchange",
+        bankOffer:true,
+        topDiscount:false,
+        image: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/2/l/g/-original-imahcqupncz6d9zz.jpeg?q=70"
+    },
+    {
+        id:2,
+        name:"MOTOROLA Razr 60 (PANTONE Lightest Sky, 256 GB)",
+        rating:4.6,
+        ratingCount:20358,
+        reviewsCount:1191,
+        specs:[
+            "6 GB RAM | 256 GB ROM",
+            "17.53 cm (6.9 inch) Full HD+ Display",
+            "50MP + 13MP | 32MP Front Camera",
+            "4500 mAh Battery",
+            "Dimensity 7400X Processor",
+            "1 Year Warranty on Handset and 6 Months Warranty on Accessories"
+        ],
+        originalPrice: 54999,
+        discountedPrice: 49999,
+        discount: "9% off",
+        freeDelivery: true,
+        exchangeOffer: "Upto ₹41,000 Off on Exchange",
+        bankOffer:true,
+        topDiscount:false,
+        image: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/2/z/b/-original-imahcquphydfhcq8.jpeg?q=70"
+    },
+    {
+        id:3,
+        name:"MOTOROLA Edge 60 Fusion 5G (PANTONE Amazonite, 256 GB)",
+        rating:4.6,
+        ratingCount:20358,
+        reviewsCount:1191,
+        specs:[
+            "8 GB RAM | 256 GB ROM",
+            "16.94 cm (6.67 inch) Full HD+ Display",
+            "50MP + 13MP | 32MP Front Camera",
+            "5500 mAh Battery",
+            "Dimensity 7400 Processor",
+            "1 Year Warranty on Handset and 6 Months Warranty on Accessories"
+        ],
+        originalPrice: 25999,
+        discountedPrice: 22999,
+        discount: "11% off",
+        freeDelivery: true,
+        exchangeOffer: "Upto ₹17,150 Off on Exchange",
+        bankOffer:true,
+        topDiscount:false,
+        image: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/j/x/j/-original-imahbfmdgpjvgecg.jpeg?q=70"
+    },
+    {
+        id:4,
+        name:"SAMSUNG Galaxy F16 5G (Glam Green, 128 GB)",
+        rating:4.2,
+        ratingCount:1324,
+        reviewsCount:998,
+        specs:[
+            "4 GB RAM | 128 GB ROM",
+            "16.91 cm (6.657 inch) Full HD+ Display",
+            "50MP + 5MP + 2MP| 13MP Front Camera",
+            "5000 mAh Battery",
+            "Dimensity 6300 Processor",
+            "1 Year Manufacturer Warranty for Device and 6 Months for In-Box Accessories"
+        ],
+        originalPrice: 11999,
+        discountedPrice: 15999,
+        discount: "25% off",
+        freeDelivery: true,
+        exchangeOffer: "Upto ₹17,150 Off on Exchange",
+        bankOffer:true,
+        topDiscount:false,
+        image: "https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fa_9e47c1.png"
+    },
+
+];
